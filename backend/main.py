5b851f19d7b19617dd9dd97ae439c3b5b226b434
pickle244 <139569506+pickle244@users.noreply.github.com>
2026-05-21 20:19:24 -0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -28,7 +28,7 @@ app.add_middleware(
 
     allow_origins=[
         "http://localhost:3000",
-        "https://nba-live-odds-rho.vercel.app/"
+        "https://nba-live-odds-rho.vercel.app"
     ],
 
     allow_credentials=True,
